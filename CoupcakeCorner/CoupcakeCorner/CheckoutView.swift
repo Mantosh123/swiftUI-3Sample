030920f43223df72066557f6e8a89d1c31319c09
Mantosh Kumar <kumar.mantosh83@gmail.com>
2024-09-28 23:21:39 +0530

    add place order logic

--- CoupcakeCorner/CoupcakeCorner/CheckoutView.swift
+++ CoupcakeCorner/CoupcakeCorner/CheckoutView.swift
@@ -11,22 +11,64 @@ struct CheckoutView: View {
     
     @Bindable var order: Order
     
+    @State private var orderMessage = ""
+    @State private var showconfirmation = false
+    
     var body: some View {
-        Form {
-            Section {
-               
-            }
-            
-            Section {
-                
-                
-                Button {
-                    //
-                } label: {
-                    Text("Check out")
+        
+        NavigationStack {
+            ScrollView {
+                VStack {
+                    AsyncImage(url: URL(string: "https://hws.dev/img/cupcakes@3x.jpg"), scale: 3) { image in
+                        image
+                    } placeholder: {
+                        ProgressView()
+                    }.frame(height: 200)
+                    
+                    Text("Your total order amount: \(order.cost, format: .currency(code: "IND"))")
+                        .bold()
+                        .padding()
+                    
+                    Button("Place order", action: {
+                        Task {
+                           await placeOrder()
+                        }
+                    }).padding()
+                    
+                }.padding()
+            }.navigationTitle("Checkout")
+                .navigationBarTitleDisplayMode(.inline)
+                .scrollBounceBehavior(.basedOnSize)
+                .alert("Thank you! ", isPresented: $showconfirmation) {
+                    
+                }message: {
+                    Text(orderMessage.localizedLowercase)
                 }
-
-            }
+        }
+    }
+    func placeOrder() async {
+        
+        guard let encoding = try? JSONEncoder().encode(order) else {
+            debugPrint("faild to encode")
+            return
+        }
+        
+        let url = URL(string: "https://reqres.in/api/cupcakes")!
+        var request = URLRequest(url: url)
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.httpMethod = "POST"
+        
+        do {
+            
+            let (data, _ ) = try await URLSession.shared.upload(for: request, from: encoding)
+            
+            let decodedOrder = try JSONDecoder().decode(Order.self, from: data)
+            
+            orderMessage = "Your order for \(decodedOrder.quantity) x \(Order.types[decodedOrder.type].lowercased()) cupcakes is on the way"
+            showconfirmation = true
+            
+        } catch {
+            print("Somthing went wrong ")
         }
     }
 }
--- CoupcakeCorner/CoupcakeCorner/Order.swift
+++ CoupcakeCorner/CoupcakeCorner/Order.swift
@@ -8,8 +8,20 @@
 import Foundation
 
 @Observable
-class Order {
+class Order: Codable {
     
+    enum CodingKeys: String, CodingKey {
+        case _type = "type"
+        case _quantity = "quantity"
+        case _specialRequestEnable = "specialRequestEnable"
+        case _expraFrosting = "expraFrosting"
+        case _addSprinkless = "addSprinkless"
+        case _name = "name"
+        case _streetAddress = "streetAddress"
+        case _city = "city"
+        case _zip = "zip"
+        
+    }
     static let types = ["Vanilla", "Strowberry", "Chocolate", "Rainbow"]
     
     var type = 0
@@ -29,6 +41,25 @@ class Order {
             return false
         }
         return true
+    }
+    
+    var cost: Decimal {
+        
+        // $2 per cake
+        var cost = Decimal(quantity) * 2
+        
+        cost = Decimal(type) / 2
+        
+        if expraFrosting {
+            cost += Decimal(quantity)
+        }
+        
+        if addSprinkless {
+            cost += Decimal(quantity)/2
+        }
+        
+        return cost
+        
     }
     
 }
